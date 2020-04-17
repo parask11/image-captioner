00cfb0976ead65ac940f9998a318a4dbd3d9b63a
Paras Kaushik <40833928+parask11@users.noreply.github.com>
2020-04-18 02:42:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,redirect, request
+import generate_captions
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/',methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        f = request.files['userfile']
+        path = "./static/{}".format(f.filename)
+        f.save(path)
+
+        c = generate_captions.give_caption(path)
+        caption = ""
+        caption += c[0].upper()
+        for i in range(1,len(c)):
+            caption += c[i]
+        caption += "."
+
+        result_dic = {
+            "image":path,
+            'caption':caption
+        }
+        #print(caption)
+    return render_template("index.html",your_result=result_dic)
+
+if __name__=="__main__":
+    app.debug = True
+    app.run()
