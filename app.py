249a0d793a3dad0e38c41df562f40fb85d45947f
paraskaushik <paraskaushik10079@gmail.com>
2020-05-01 13:39:53 +0530

    Adding more captions

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template,redirect, request
 import generate_captions
+import generate_captions2
+import generate_captions3
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,19 +15,37 @@ def submit():
         path = "./static/{}".format(f.filename)
         f.save(path)
 
-        c = generate_captions.give_caption(path)
-        caption = ""
-        caption += c[0].upper()
-        for i in range(1,len(c)):
-            caption += c[i]
-        caption += "."
+        c1 = generate_captions.give_caption(path)
+        caption1 = ""
+        caption1 += c1[0].upper()
+        for i in range(1,len(c1)):
+            caption1 += c1[i]
+        caption1 += "."
+
+        c2 = generate_captions2.give_caption(path)
+        caption2 = ""
+        caption2 += c2[0].upper()
+        for i in range(1, len(c2)):
+            caption2 += c2[i]
+        caption2 += "."
+
+        c3 = generate_captions3.give_caption(path)
+        caption3 = ""
+        caption3 += c3[0].upper()
+        for i in range(1, len(c3)):
+            caption3 += c3[i]
+        caption3 += "."
 
         result_dic = {
             "image":path,
-            'caption':caption
+            'caption1':caption1,
+            'caption2': caption2,
+            'caption3': caption3
         }
-        #print(caption)
-    return render_template("index.html",your_result=result_dic, file_name=f.filename)
+        print(caption1)
+        print(caption2)
+        print(caption3)
+    return render_template("index.html",your_result=result_dic)
 
 if __name__=="__main__":
     app.debug = True
--- generate_captions.py
+++ generate_captions.py
@@ -2,26 +2,26 @@ import numpy as np
 import pickle
 from keras.applications.resnet50 import ResNet50, preprocess_input
 from keras.preprocessing import image
-from keras.models import Model, load_model
+from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Input, Dense, Dropout, Embedding, LSTM
 from keras.layers.merge import add
 
-
-model = ResNet50(weights="imagenet",input_shape=(3,224,224))
-model_new = Model(input=model.input,output = model.layers[-2].output)
+model = ResNet50(weights="imagenet", input_shape=(3, 224, 224))
+model_new = Model(input=model.input, output=model.layers[-2].output)
 model_new._make_predict_function()
+
 
 def predict_captions(photo):
     in_text = "<s>"
     for i in range(max_len):
         sequence = [word_to_index[w] for w in in_text.split() if w in word_to_index]
-        sequence = pad_sequences([sequence],maxlen=max_len,padding='post')
+        sequence = pad_sequences([sequence], maxlen=max_len, padding='post')
 
-        ypred = model.predict([photo,sequence])
+        ypred = model.predict([photo, sequence])
         ypred = ypred.argmax()
         word = index_to_word[ypred]
-        in_text += ' '+word
+        in_text += ' ' + word
         if word == "<e>":
             break
     final_caption = in_text.split()[1:-1]
@@ -29,13 +29,15 @@ def predict_captions(photo):
 
     return final_caption
 
+
 def preprocess_img(img):
-    img = image.load_img(img,target_size=(224,224))
+    img = image.load_img(img, target_size=(224, 224))
     img = image.img_to_array(img)
-    img = np.expand_dims(img,axis=0)
-    
+    img = np.expand_dims(img, axis=0)
+
     img = preprocess_input(img)
     return img
+
 
 def encode_img(img):
     img = preprocess_img(img)
@@ -43,47 +45,69 @@ def encode_img(img):
     feature_vector = feature_vector.reshape((-1,))
     return feature_vector
 
-max_len=74
-vocab_size=5121
+
+max_len = 74
+vocab_size = 5121
 
 embedding_matrix = None
-with open("embedding_matrix.pkl",'rb') as f:
-       embedding_matrix = pickle.load(f)
+with open("embedding_matrix.pkl", 'rb') as f:
+    embedding_matrix = pickle.load(f)
 
 word_to_index = None
-with open("word_to_index.pkl",'rb') as f:
-       word_to_index = pickle.load(f)
+with open("word_to_index.pkl", 'rb') as f:
+    word_to_index = pickle.load(f)
 
 index_to_word = None
-with open("index_to_word.pkl",'rb') as f:
-       index_to_word = pickle.load(f)
+with open("index_to_word.pkl", 'rb') as f:
+    index_to_word = pickle.load(f)
 
 input_img_features = Input(shape=(2048,))
 inp_img1 = Dropout(0.3)(input_img_features)
-inp_img2 = Dense(256,activation='relu')(inp_img1)
+inp_img2 = Dense(256, activation='relu')(inp_img1)
 
 input_captions = Input(shape=(max_len,))
-inp_cap1 = Embedding(input_dim=vocab_size,output_dim=50,mask_zero=True)(input_captions)
+inp_cap1 = Embedding(input_dim=vocab_size, output_dim=50, mask_zero=True)(input_captions)
 inp_cap2 = Dropout(0.3)(inp_cap1)
 inp_cap3 = LSTM(256)(inp_cap2)
 
-decoder1 = add([inp_img2,inp_cap3])
-decoder2 = Dense(256,activation='relu')(decoder1)
-outputs = Dense(vocab_size,activation='softmax')(decoder2)
+decoder1 = add([inp_img2, inp_cap3])
+decoder2 = Dense(256, activation='relu')(decoder1)
+outputs = Dense(vocab_size, activation='softmax')(decoder2)
 
-model = Model(inputs=[input_img_features,input_captions],outputs=outputs)
+model = Model(inputs=[input_img_features, input_captions], outputs=outputs)
 
 model.layers[2].set_weights([embedding_matrix])
 model.layers[2].trainable = False
 
-
-#model.load_weights("model_weights/model_29.h5")
-
 model.load_weights("model_37.h5")
-
 model._make_predict_function()
 
+# In[66]:
+
+
+# img_path = "/Users/paraskaushik/Desktop/FOOD/CODES/DATA_SCIENCE/Image Captioning Bot/8k/flickr8k/Flickr_Data/Flickr_Data/Images/"
+
+
+# In[67]:
+
+
+"""for i in range(15):
+    id = np.random.randint(0,1000)
+    all_image_names = list(os.listdir(img_path))
+    img_name = all_image_names[id]
+    photo_2048 = encode_img(img_path+img_name).reshape((1,2048))
+    #photo_2048 = encoding_train[img_name].reshape((1,2048))
+
+    i = plt.imread(img_path+img_name)
+    caption = predict_captions(photo_2048)
+    print(caption)
+
+    plt.imshow(i)
+    plt.axis("off")
+    plt.show()"""
+
+
 def give_caption(image_path):
-    photo_2048 = encode_img(image_path).reshape((1,2048))
+    photo_2048 = encode_img(image_path).reshape((1, 2048))
     caption = predict_captions(photo_2048)
     return caption
